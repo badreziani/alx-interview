89806ebb0ce3e85e84860b8961f7a1a6aa4c39c0
Badre Ziani <badreziani@gmail.com>
2024-11-14 19:02:46 +0100

    Create starwars charachers project

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+request(url, function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
